d55bfc3c0adfb1e23d5eae06341d2b2f46c7a566
Damir <dydamirbey@gmail.com>
2019-04-22 17:40:30 -0400

    Setting up Teams API

--- ASSIGNMENT 1/Assignment 1/js/main.js
+++ ASSIGNMENT 1/Assignment 1/js/main.js
@@ -1,13 +1,3 @@
-/*********************************************************************************
-* WEB422 – Assignment 1
-* I declare that this assignment is my own work in accordance with Seneca Academic Policy.
-* No part of this assignment has been copied manually or electronically from any other source
-* (including web sites) or distributed to other students.
-*
-* Name: _DAMIRKHON YODGOROV_ Student ID: _108364175_ Date: 15.01.2019_
-*
-*
-********************************************************************************/ 
 $(document).ready(function(){
 
     //TEAMS BUTTON
