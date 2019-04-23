746717345d41ac3eabc87627949b3d0e7b072cc1
Damir <dydamirbey@gmail.com>
2019-04-23 13:36:49 -0400

    Update main.js

--- ASSIGNMENT 2/Web422_assignment 2/js/main.js
+++ ASSIGNMENT 2/Web422_assignment 2/js/main.js
@@ -1,16 +1,3 @@
-/*********************************************************************************
-* WEB422 – Assignment 2
-* I declare that this assignment is my own work in accordance with Seneca Academic Policy.
-* No part of this assignment has been copied manually or electronically from any other source
-* (including web sites) or distributed to other students.
-*
-* Name: _Damirkhon Yodgorov_ Student ID: _108364175_ Date: 31.01.2019
-*
-*
-********************************************************************************/ 
-
-
-
 let employeesModel=[]; // global array used to store all employees received from teams-api
 
 // initializeEmployeesModel function
