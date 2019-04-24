cee721a4188684d22a3b20991939fe854b62560f
Damir <dydamirbey@gmail.com>
2019-04-24 11:34:15 -0400

    Update main.js

--- REACT Application/Web422_assignment 3/js/main.js
+++ REACT Application/Web422_assignment 3/js/main.js
@@ -1,14 +1,3 @@
-/*********************************************************************************
-* WEB422 – Assignment 03
-* I declare that this assignment is my own work in accordance with Seneca Academic Policy. No part of this
-* assignment has been copied manually or electronically from any other source (including web sites) or
-* distributed to other students.
-*
-* Name: __DAMIRKHON YODGOROV__ Student ID: __108364175__ Date: __12.02.2019__
-*
-********************************************************************************/ 
-
-
 /**********************************  VIEW MODEL DECLARATION AND DEFINITION ************************************************************* */
 var viewModel={
      teams:ko.observableArray([]),
