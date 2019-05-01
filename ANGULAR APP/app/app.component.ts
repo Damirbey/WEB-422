3c0a1d99e80477471624d64d0346f3eb38c53ccf
Damir <dydamirbey@gmail.com>
2019-04-30 22:56:26 -0400

    ANGULAR Application, Milestone 2

--- ANGULAR APP/app/app.component.ts
+++ ANGULAR APP/app/app.component.ts
@@ -1,13 +1,3 @@
-/*********************************************************************************
-* WEB422 – Assignment 05
-* I declare that this assignment is my own work in accordance with Seneca Academic Policy. No part of this
-* assignment has been copied manually or electronically from any other source (including web sites) or
-* distributed to other students.
-*
-* Name: _Damirkhon Yodgorov_ Student ID: _108364175_ Date: _18.03.2019_
-*
-********************************************************************************/ 
-
 import { Component } from '@angular/core';
 
 @Component({
