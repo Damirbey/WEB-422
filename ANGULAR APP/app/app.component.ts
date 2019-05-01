36ad5334b0831a5ae2e72528cce13b0950351c8d
Damir <dydamirbey@gmail.com>
2019-05-01 07:46:56 -0400

    ANGULAR Application , Milestone 3

--- ANGULAR APP/app/app.component.ts
+++ ANGULAR APP/app/app.component.ts
@@ -1,13 +1,3 @@
-/********************************************************************************* * 
- *  WEB422 – Assignment 06 *  
- * I declare that this assignment is my own work in accordance with Seneca  Academic Policy.  
- * No part of this *  assignment has been copied manually or electronically from any other source (including web sites) or  * 
- *  distributed to other students. *  
- *  
- *  Name: _Damirkhon Yodgorov_ Student ID: _108364175_   Date: 2019-04-04 * 
- *   
- * Heroku Link: https://arcane-falls-63252.herokuapp.com/ 
- * * ********************************************************************************/  
 
 import { Component } from '@angular/core';
 
